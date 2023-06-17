17bb0fd362c5632af7852e32b2e1b453962970c4
omrajsharma <omraj7413@gmail.com>
2023-06-17 21:41:18 +0530

    feat: user login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,31 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const User = require('../models/UserModel');
 
 const salt = bcrypt.genSaltSync(10);
 
 const register = async (req, res) => {
     try {
-        console.log('body', req.body);
         const { username, password } = req.body;
+
+        if (!username || !password) {
+            res.status(400).end('Username or password is missing');
+            return;
+        }
+        if (username.length < 3) {
+            res.status(400).end('Username must be at least 3 characters');
+            return;
+        }
+        if (username.length > 20) {
+            res.status(400).end('Username must be less than 20 characters');
+            return;
+        }
+        if (password.length < 8) {
+            res.status(400).end('Password must be at least 8 characters');
+            return;
+        }
+
         const user = await User.create({ username, 
             password : bcrypt.hashSync(password, salt)
         });
@@ -16,4 +35,42 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { register };
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).end('Username or password is missing');
+        return;
+    }
+    if (username.length < 3) {
+        res.status(400).end('Username must be at least 3 characters');
+        return;
+    }
+    if (username.length > 20) {
+        res.status(400).end('Username must be less than 20 characters');
+        return;
+    }
+    if (password.length < 8) {
+        res.status(400).end('Password must be at least 8 characters');
+        return;
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        res.status(400).end('User not found');
+        return;
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordCorrect) {
+        res.status(400).end('Password is incorrect');
+        return;
+    }
+
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.cookie('token', token, { httpOnly: true }).status(200).end('Logged in');
+}
+
+module.exports = { register, login };
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/AuthController');
+const { register, login } = require('../controllers/AuthController');
 
+// /api/v1/auth/register
 router.post('/register', register);
+
+// /api/v1/auth/login
+router.post('/login', login);
 
 module.exports = router;
